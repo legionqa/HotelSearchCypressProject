e4ea1d609d4ea7efc43ac4a594ed6407a65f25c6
Lev Gorfel <lev.gorfel@basis.net>
2022-09-12 18:15:12 -0400

    remove comments

--- cypress/e2e/job-cafe.cy.js
+++ cypress/e2e/job-cafe.cy.js
@@ -1,35 +1,35 @@
 ///<reference types = "Cypress"/>
 describe('Job Cafe sanity spec', {defaultCommandTimeout: 10000}, () => {
-    // it('test jobs page with 10 sec', {defaultCommandTimeout: 10000}, () => {
+    it('test jobs page with 10 sec', {defaultCommandTimeout: 10000}, () => {
      
-    //     cy.visit('job-page')
-    //    // cy.wait(5000)
-    //     cy.get('[class="post-item clearfix"]').should('be.visible')
+        cy.visit('job-page')
+       // cy.wait(5000)
+        cy.get('[class="post-item clearfix"]').should('be.visible')
 
-    // })
+    })
 
-    // it('test jobs page search for QA position', {defaultCommandTimeout: 20000}, () => {
+    it('test jobs page search for QA position', {defaultCommandTimeout: 20000}, () => {
      
-    //     cy.visit('job-page')
-    //     // cy.get('[placeholder="position"]').type('QA', {force: true})
-    //     // cy.get('button').contains('search').click({force: true})
-    //     cy.searchForPosition('QA')
-    //     cy.get('[class="post-item clearfix"]').contains('QA').should('be.visible')
+        cy.visit('job-page')
+        // cy.get('[placeholder="position"]').type('QA', {force: true})
+        // cy.get('button').contains('search').click({force: true})
+        cy.searchForPosition('QA')
+        cy.get('[class="post-item clearfix"]').contains('QA').should('be.visible')
 
 
-    // })
+    })
 
-    // it('test jobs page search for developer position', {defaultCommandTimeout: 20000}, () => {
+    it('test jobs page search for developer position', {defaultCommandTimeout: 20000}, () => {
      
-    //     cy.visit('job-page')
-    //     // cy.get('[placeholder="position"]').type('Developer', {force: true})
-    //     // cy.get('button').contains('search').click({force: true})
-    //     cy.searchForPosition('Developer')
+        cy.visit('job-page')
+        // cy.get('[placeholder="position"]').type('Developer', {force: true})
+        // cy.get('button').contains('search').click({force: true})
+        cy.searchForPosition('Developer')
 
-    //     cy.get('[class="post-item clearfix"]').contains('Developer').should('be.visible')
+        cy.get('[class="post-item clearfix"]').contains('Developer').should('be.visible')
 
 
-    // })
+    })
 
     it('test jobs page search for QA positions in USA By company name', {defaultCommandTimeout: 20000}, () => {
      
